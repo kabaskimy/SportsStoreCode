71d473980216b78e5e98913c0d1949c0f8747d58
kabaskimy <kabaskimy@gmail.com>
2013-02-18 17:57:05 +0800

    update to category link

---
+++ SportsStoreApp/SportsStoreApp.MVC.WebUI/Controllers/NavController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStoreApp.MVC.Domain.Abstract;
+using SportsStoreApp.MVC.Domain.Entities;
+
+namespace SportsStoreApp.MVC.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        //
+        // GET: /Nav/
+
+        public PartialViewResult  Menu(string category=null)
+        {
+            //return "Hello From Menu Action";
+            IEnumerable<string> categoryName = repository.Products.Select(m => m.Category).Distinct().OrderBy(m => m);
+            ViewBag.SelectedCategory = category;
+            return PartialView(categoryName);
+        }
+
+        private IProductRepository repository;
+        public NavController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+    }
+}
